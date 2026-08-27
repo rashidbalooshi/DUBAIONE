9d9eda5af31f512557dedd47886403147342cd26
rashidbalooshi <rashid.muhammad9@icloud.com>
2026-08-27 17:28:49 +0400

    Update widget test for premium DubaiOne home design

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,10 +2,10 @@ import 'package:dubaione/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('DubaiOne home screen loads', (tester) async {
+  testWidgets('DubaiOne premium home screen loads', (tester) async {
     await tester.pumpWidget(const DubaiOneApp());
     expect(find.text('DUBAIONE'), findsOneWidget);
-    expect(find.text('Our Services'), findsOneWidget);
+    expect(find.text('HOW CAN WE HELP YOU?'), findsOneWidget);
     expect(find.text('Golden Visa'), findsOneWidget);
   });
 }
