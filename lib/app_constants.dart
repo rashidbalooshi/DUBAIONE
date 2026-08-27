0cc276482faff1e995c5701bec0190cba881ee18
rashidbalooshi <rashid.muhammad9@icloud.com>
2026-08-27 15:47:29 +0400

    Centralize DubaiOne app constants

---
+++ lib/app_constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const appName = 'DubaiOne';
+  static const whatsappNumber = '971501768878';
+  static const displayPhone = '+971 50 176 8878';
+  static const disclaimer = 'DubaiOne is an independent assistance service and is not a UAE government authority.';
+}
